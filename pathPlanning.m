e009f5f498b831fda2c9d6d172eae7813873cec7
JeongEuiJoon <dordong327@gmail.com>
2018-05-25 11:51:14 +0200

    change zero to Inf

--- pathPlanning.m
+++ pathPlanning.m
@@ -2,12 +2,16 @@ function ListOfWp = pathPlanning(distanceMatrix,heuristic,origin,target,method)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
 ListOfWp = [];
+dummy = [];
 
 switch method
     case 0
         ListOfWp = GreedyPath(distanceMatrix,origin,target);
     case 1
+        dummy = Dijkstra(distanceMatrix,origin,target);
+        ListOfWp = dummy(1,1);
     case 2
+        dummy = 
     otherwise
 end
 
